123b7ee3e7ab3537405e3c63cfe56d3cc5a8273e
aamer Essa <aamer.es12@gmail.com>
2022-12-21 07:32:50 +0300

    Initial Commit

---
+++ To Do List v.2/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  To Do List v.2
+//
+//  Created by Aamer Essa on 21/12/2022.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
